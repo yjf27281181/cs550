f9f562018b9ce38560b8d81f1133840b475f808e
yuyingji <44735453+yuyingji@users.noreply.github.com>
2020-04-09 22:10:09 -0700

    Update proxy.cpp

--- P2/proxy.cpp
+++ P2/proxy.cpp
@@ -1,80 +1,45 @@
-//Client side C/C++ program to demonstrate Socket programming
-//#include <stdio.h>
-//#include <sys/socket.h>
-#include <arpa/inet.h>
-//#include <unistd.h>
-//#include <string.h>
-//#define PORT 6379
-//
 
-//int main(int argc, char const *argv[])
-//{
-//    int sock = 0, valread;
-//    struct sockaddr_in serv_addr;
-//    char *hello = "INFO\r\n";
-//    char buffer[1024] = {0};
-//    if ((sock = socket(AF_INET, SOCK_STREAM, 0)) < 0)
-//    {
-//        printf("\n Socket creation error \n");
-//        return -1;
-//    }
-//
-//    serv_addr.sin_family = AF_INET;
-//    serv_addr.sin_port = htons(PORT);
-//
-//    // Convert IPv4 and IPv6 addresses from text to binary form
-//    if(inet_pton(AF_INET, "127.0.0.1", &serv_addr.sin_addr)<=0)
-//    {
-//        printf("\nInvalid address/ Address not supported \n");
-//        return -1;
-//    }
-//
-//    if (connect(sock, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) < 0)
-//    {
-//        printf("\nConnection Failed \n");
-//        return -1;
-//    }
-//    send(sock , hello , strlen(hello) , 0 );
-//    printf("Hello message sent\n");
-//    valread = read( sock , buffer, 1024);
-//    printf("%s\n",buffer );
-//    return 0;
-//}
-// Server side C/C++ program to demonstrate Socket programming
+// Created by Yuying on 4/8/20.
+
 #include <unistd.h>
 #include <stdio.h>
 #include <sys/socket.h>
 #include <stdlib.h>
 #include <netinet/in.h>
 #include <string.h>
-#define PORT 8080
+#include <arpa/inet.h>
+#include <thread>
+
+void task1(int client_socket, char* buffer, char* from_redis, int redis_sock) {
+    while (1) {
+        int from_app_len = read(client_socket, buffer, 10240);
+        send(redis_sock, buffer, from_app_len, 0);
+        int from_redis_len = read(redis_sock, from_redis, 10240);
+        int to_app_len = send(client_socket, from_redis, from_redis_len, 0);
+    }
+}
+
 int main(int argc, char const *argv[]) {
-    int server_fd, client_socket, from_app_len;
+    int server_fd, client_socket;
     struct sockaddr_in address;
-    int opt = 1;
     int addrlen = sizeof(address);
-    char buffer[1024] = {0}, from_redis[1024] = {0};
+    char buffer[10240] = {0}, from_redis[10240] = {0};
 
-
-    // Creating socket file descriptor
     if ((server_fd = socket(AF_INET, SOCK_STREAM, 0)) == 0) {
         perror("socket failed");
         exit(EXIT_FAILURE);
     }
 
-    // Forcefully attaching socket to the port 8080
-
     address.sin_family = AF_INET;
     address.sin_addr.s_addr = INADDR_ANY;
-    address.sin_port = htons(PORT);
+    address.sin_port = htons(atoi(argv[1]));
 
-    // Forcefully attaching socket to the port 8080
     if (bind(server_fd, (struct sockaddr *) &address,
              sizeof(address)) < 0) {
         perror("bind failed");
         exit(EXIT_FAILURE);
     }
-    if (listen(server_fd, 3) < 0) {
+    if (listen(server_fd, 1000) < 0) {
         perror("listen");
         exit(EXIT_FAILURE);
     }
@@ -85,34 +50,28 @@ int main(int argc, char const *argv[]) {
             exit(EXIT_FAILURE);
         }
 
-        from_app_len = read(client_socket, buffer, 1024);
-
-        printf("%d - %s\n", from_app_len, buffer);
-
-
         int redis_sock = 0;
         struct sockaddr_in serv_addr;
         if ((redis_sock = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
             printf("\n Socket creation error \n");
-            return -1;
+            exit(EXIT_FAILURE);
         }
 
         serv_addr.sin_family = AF_INET;
-        serv_addr.sin_port = htons(6379);
+        serv_addr.sin_port = htons(atoi(argv[2]));
 
-        // Convert IPv4 and IPv6 addresses from text to binary form
         if (inet_pton(AF_INET, "127.0.0.1", &serv_addr.sin_addr) <= 0) {
             printf("\nInvalid address/ Address not supported \n");
-            return -1;
+            exit(EXIT_FAILURE);
         }
 
         if (connect(redis_sock, (struct sockaddr *) &serv_addr, sizeof(serv_addr)) < 0) {
             printf("\nConnection Failed \n");
-            return -1;
+            exit(EXIT_FAILURE);
         }
-        send(redis_sock, buffer, from_app_len, 0);
-        int from_redis_len = read(redis_sock, from_redis, 1024);
-        int to_app_len = send(client_socket, from_redis, from_redis_len, 0);
+
+        std::thread t (task1, client_socket, buffer, from_redis, redis_sock);
+        t.detach();
 
     }
 }
